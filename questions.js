88f19bd497019bcb83f5d01042f5aa90a47e6a6d
deva005 <122748280+deva005@users.noreply.github.com>
2023-02-23 14:30:11 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "JPEG stands for.?",
+    answer: "Joint photographic experts group",
+    options: [
+      "Joint photographic embedded group",
+      "Joint photographic experts group",
+      "Joint photographic extended group",
+      "none of the above"
+    ]
+  },
+  {
+    numb: 7,
+    question: "The purpose of markup is to.",
+    answer: "both A & B",
+    options: [
+      "add hypertext capabilities",
+      "enhance the document",
+      "both A & B",
+      "none of the above"
+    ]
+  },
+  {
+    numb: 8,
+    question: "For a particular font its size attribute can be an absolute value ranging form.",
+    answer: "1-10",
+    options: [
+      "1-10",
+      "1-9",
+      "1-8",
+      "1-7"
+    ]
+  },
+  {
+    numb: 9,
+    question: "The Major components of the Web browser are?",
+    answer: "All Of the Above",
+    options: [
+      "Menu Bar",
+      "ToolBar",
+      "Location",
+      "All Of the Above"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which one of the following tags is used to insert graphics in the webpage?",
+    answer: "img",
+    options: [
+      "image",
+      "img",
+      "images",
+      "graphics"
+    ]
+  },
+];
